b7990acb0a6aa5d6abf09159af23553f65dad193
leandro balmaceda <leandro20.balmaceda@gmail.com>
2024-04-01 14:47:30 -0300

    fix nav list

--- src/components/navbar/NavListDrawer.jsx
+++ src/components/navbar/NavListDrawer.jsx
@@ -2,61 +2,54 @@ import React from 'react';
 import { Box, Button, Typography } from "@mui/material";
 import productos from '../../productos.json'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import CloseIcon from '@mui/icons-material/Close';
 
-const NavListDrawer = ({ setOpen,handleTypeSelection }) => {
-  const tiposUnicos = Array.from(new Set(Object.keys(productos.productos)));
+const NavListDrawer = ({ setOpen, handleTypeSelection }) => {
+  // Crear un conjunto para almacenar tipos únicos
+  const tiposUnicos = new Set();
+
+  // Iterar sobre los productos y agregar los tipos únicos al conjunto
+  Object.values(productos.productos).forEach(productosTipo => {
+    productosTipo.forEach(producto => {
+      tiposUnicos.add(producto.tipo);
+    });
+  });
 
   return (
     <>
-      <Box sx={{borderBottom:'2px solid #FF0000'}}>
-       <Typography variant='h5' textAlign={'center'} color={'#ff0000'} bgcolor={'black'} height={'3rem'} lineHeight={'3rem'} onClick={()=>setOpen(false)}>Cerrar </Typography>
-       <hr />
-        {tiposUnicos.map((tipo, index) => (
-          <React.Fragment key={index}>
-            <Typography
-              variant="h5"
-              textAlign={"center"}
-              fontWeight={"bold"}
-              p={'.4rem'}
-              textTransform='capitalize'
-            >
-              {tipo}
-            </Typography>
-            {productos.productos[tipo].map((producto, productoIndex) => (
-              <Button
-                key={productoIndex}
-                sx={{
-                  width: "100%",
-                  textTransform: "capitalize",
-                  bgcolor: "#F2F3F4",
-                  color: "#626567 ",
-                  borderBottom: "1px solid #D7DBDD",
-                  fontSize: "1rem",
-                }}
-                onClick={() =>{ setOpen(false),handleTypeSelection(producto.tipo)}}
-                
-              >
-                {producto.tipo}
-              </Button>
-            ))}
-          </React.Fragment>
+      <Box sx={{ borderBottom: '2px solid #FF0000' }}>
+        <Typography variant='h5' textAlign={'center'} color={'#ff0000'} bgcolor={'black'} height={'3rem'} lineHeight={'3rem'} onClick={() => setOpen(false)}>Cerrar </Typography>
+        <hr />
+        {[...tiposUnicos].map((tipo, index) => (
+          <Button
+            key={index}
+            sx={{
+              width: "100%",
+              textTransform: "capitalize",
+              bgcolor: "#F2F3F4",
+              color: "#626567 ",
+              borderBottom: "1px solid #D7DBDD",
+              fontSize: "1rem",
+            }}
+            onClick={() => { setOpen(false), handleTypeSelection(tipo) }}
+          >
+            {tipo}
+          </Button>
         ))}
         <Button
-        sx={{
-          width: "100%",
-          textTransform: "capitalize",
-          bgcolor: "black",
-          color: "#ff0000",
-          borderBottom: "1px solid #D7DBDD",
-          fontSize: "1rem",
-          borderRadius:'0px'
-        }}
-        onClick={()=>{setOpen(false),handleTypeSelection(null)}}
-      >
-        <ArrowBackIcon/>
-        Volver al Inicio
-      </Button>
+          sx={{
+            width: "100%",
+            textTransform: "capitalize",
+            bgcolor: "black",
+            color: "#ff0000",
+            borderBottom: "1px solid #D7DBDD",
+            fontSize: "1rem",
+            borderRadius: '0px'
+          }}
+          onClick={() => { setOpen(false), handleTypeSelection(null) }}
+        >
+          <ArrowBackIcon />
+          Volver al Inicio
+        </Button>
       </Box>
     </>
   );

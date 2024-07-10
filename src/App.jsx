0005d843534508db56e839dd9cfd95bdf66158a5
leandro balmaceda <leandro20.balmaceda@gmail.com>
2024-07-10 20:14:15 -0300

    new web

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ import { AuthProvider } from "./Contexts/AuthContext.jsx"; // Importa el AuthPro
 import { supabase } from "./supabaseClient";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { ThemeProvider } from "@mui/material";
-import theme from './theme/theme.js'
-import Preloader from './components/Preloader.jsx'; // Importa el Preloader
+import { Box, Button, ThemeProvider, Typography } from "@mui/material";
+import theme from "./theme/theme.js";
+import Preloader from "./components/Preloader.jsx"; // Importa el Preloader
+import { Height } from "@mui/icons-material";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -34,30 +35,62 @@ function App() {
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <CartProvider>
-          {loading && <Preloader loading={loading} />}
-          <BrowserRouter>
-            <Header />
-            <Routes>
-              <Route
-                path="/"
-                element={<Home products={products} loading={loading} />}
-              />
-              <Route
-                path="/detalle/:codigo"
-                element={<Detalle products={products} loading={loading} />}
-              />
-              <Route path="/envios" element={<Envios />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/add" element={<ProtectedRoute element={<Add />} />} />
-            </Routes>
-            <Footer />
-          </BrowserRouter>
-        </CartProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <>
+      <Box
+        sx={{
+          maxWidth: "30rem",
+          height: "10rem auto",
+          width: "90%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          m:'10rem auto',
+          outline:'1px solid white',
+          p:'.5rem',
+          borderRadius:'.5rem'
+        }}
+      >
+        {" "}
+        <Typography
+          variant="h1"
+          fontSize="3rem"
+          color={"white"}
+          textAlign={"center"}
+          fontWeight={'bold'}
+        >
+          Visita nuestra nueva web!
+        </Typography>
+        <Button sx={{ bgcolor: "green",color:'white',mt:'2rem'}} href="https://raptorstore4.mitiendanube.com/" target="blank">Click aca</Button>
+      </Box>
+
+      {
+        //     <ThemeProvider theme={theme}>
+        //   <AuthProvider>
+        //     <CartProvider>
+        //       {loading && <Preloader loading={loading} />}
+        //       <BrowserRouter>
+        //         <Header />
+        //         <Routes>
+        //           <Route
+        //             path="/"
+        //             element={<Home products={products} loading={loading} />}
+        //           />
+        //           <Route
+        //             path="/detalle/:codigo"
+        //             element={<Detalle products={products} loading={loading} />}
+        //           />
+        //           <Route path="/envios" element={<Envios />} />
+        //           <Route path="/login" element={<Login />} />
+        //           <Route path="/add" element={<ProtectedRoute element={<Add />} />} />
+        //         </Routes>
+        //         <Footer />
+        //       </BrowserRouter>
+        //     </CartProvider>
+        //   </AuthProvider>
+        // </ThemeProvider>
+      }
+    </>
   );
 }
 
